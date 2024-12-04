9f32b013f9156640f42ad0e09ac2f0603c1553d8
Bi-02 <d.word2015@mail.ru>
2024-12-04 18:25:31 +0200

    some fix

--- src/bot.py
+++ src/bot.py
@@ -6,10 +6,12 @@ import datetime
 import json
 
 bot = telebot.TeleBot(setings.BOT_TOKEN)
-quotes = {}
-user_data = {}
-tea_stats = {}
 
+# Словари для хранения данных пользователей и цитат
+user_data = {}
+quotes = []
+
+# Функции для загрузки и сохранения данных
 def load_user_data():
     global user_data
     try:
@@ -28,70 +30,22 @@ def load_quotes():
         with open('quotes.json', 'r', encoding='utf-8') as file:
             quotes = json.load(file)
     except FileNotFoundError:
-        quotes = {}
+        quotes = []
 
 def save_quotes():
     with open('quotes.json', 'w', encoding='utf-8') as file:
         json.dump(quotes, file, ensure_ascii=False, indent=4)
 
-
-def add_user(chat_id, user):
-    global user_data 
-    if chat_id not in user_data:
-        user_data[chat_id] = []
-
-    if not any(x['id'] == user.id for x in user_data[chat_id]):
-        user_data[chat_id].append({'id': user.id, 'name': user.first_name or 'nn'}) 
-
-
-
-@bot.my_chat_member_handler()
-
-def drink_tea(chat_id, user_id, user_name):
-    global tea_stats
-    if chat_id not in tea_stats:
-        tea_stats[chat_id] = {}
-
-    if user_id not in tea_stats[chat_id]:
-        tea_stats[chat_id][user_id] = {
-            'name': user_name,
+# Функция для добавления пользователя
+def add_user(user):
+    user_id = str(user.id)
+    if user_id not in user_data:
+        user_data[user_id] = {
+            'name': user.first_name or 'nn',
             'tea_drink': 0,
             'kettle_failed': 30
         }
-    if tea_stats[chat_id][user_id]['kettle_failed'] > 0:
-        if random.randint(1, 100) <= 25:
-            tea_stats[chat_id][user_id]['kettle_failed'] -= 1
-            if tea_stats[chat_id][user_id]['kettle_failed'] == 0:
-                return f'ТЫ БЫЛ ИЗБРАННИКОМ! ВСЕ ДУМАЛИ ТЫ БУДЕШЬ ЗАЩИЩАТЬ НАС ОТ ЖЕНЩИН А НЕ ПРИМКНЕШЬ К НИМ!'
-            return f'чайник не вскипел🤬смотри что бы девушки не появилось. осталось не выпетых чаев: {tea_stats[chat_id][user_id]['kettle_failed']}'
-        
-        tea_amount = random.randint(1, 300)
-        tea_stats[chat_id][user_id]['tea_drink'] += tea_amount
-        if tea_amount <= 150:
-            return (
-                f'ты випил {tea_amount} чая \nвыпито чая всего: {tea_stats[chat_id][user_id]['tea_drink']}\nосталось не выпитых чаев: {tea_stats[chat_id][user_id]['kettle_failed']}'
-            )
-        else:
-            return (
-                f' ОМАГАД ТЫ ВИПИЛ АЖ {tea_amount} Л ЧАЯ!!!\nвыпито чая всего: {tea_stats[chat_id][user_id]['tea_drink']}\nосталось не выпитых чаев: {tea_stats[chat_id][user_id]['kettle_failed']}'
-            )
-    else:
-        return f'тебе уже нет смысла пить чай ситх...'
-
-@bot.message_handler(func=lambda message: message.text.lower() == 'выпить чай')
-def tea(message: types.Message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    user_name = message.from_user.username
-    response = drink_tea(chat_id, user_id, user_name)
-    bot.reply_to(message, response)
-
-# @bot.message_handler(func=lambda message: True)
-# def save_user(message:types.Message):
-#     chat_id = message.chat.id
-#     user = message.from_user
-#     add_user(chat_id, user)
-
+# Обработка статуса бота в чате
 @bot.my_chat_member_handler()
 def check_admin(message:types.ChatMemberUpdated):
     if message.chat.type in ['group', 'supergroup']:
@@ -99,47 +53,85 @@ def check_admin(message:types.ChatMemberUpdated):
         new_status = message.new_chat_member.status
         if old_status != 'administrator' and new_status == 'administrator':
             bot.send_message(chat_id=message.chat.id, text= 'МУ ХА ХА ХА ХА Я ТЕПЕРЬ АДМИН Я ВАС КАК ГОДЖО РАТАТАТААТААТТ\nА ЧТО БЫ УЗНАТЬ ВСЕ МОИ ВИДЫ РАССТРЕЛА НАПИШИ /info')
-    
+
+#Обработка запроса команды "Выпить чай"
+@bot.message_handler(func=lambda message: message.text and message.text.lower() == 'выпить чай')
+def tea(message: types.Message):
+    user = message.from_user
+    user_id = str(user.id)
+    add_user(user)
+
+    user_info = user_data[user_id]
+
+    if user_info['kettle_failed'] > 0:
+        if random.randint(1, 100) <= 25:
+            user_info['kettle_failed'] -= 1
+            if user_info['kettle_failed'] == 0:
+                response = 'ТЫ БЫЛ ИЗБРАННИКОМ! ВСЕ ДУМАЛИ, ЧТО ТЫ БУДЕШЬ ЗАЩИЩАТЬ НАС ОТ ЖЕНЩИН, А НЕ ПРИМКНЕШЬ К НИМ!'
+            else:
+                response = f'Чайник не вскипел 🤬 Смотри, чтобы девушка не появилась. Осталось не выпитых чаёв: {user_info["kettle_failed"]}'
+        else:
+            tea_amount = random.randint(1, 300)
+            user_info['tea_drink'] += tea_amount
+            if tea_amount <= 150:
+                response = (
+                    f'Ты выпил {tea_amount} чая\nВыпито чая всего: {user_info["tea_drink"]}\nОсталось не выпитых чаёв: {user_info["kettle_failed"]}'
+                )
+            else:
+                response = (
+                    f'ОМАГАД ТЫ ВЫПИЛ АЖ {tea_amount} Л ЧАЯ!!!\nВыпито чая всего: {user_info["tea_drink"]}\nОсталось не выпитых чаёв: {user_info["kettle_failed"]}'
+                )
+    else:
+        response = 'Тебе уже нет смысла пить чай, ситх...'
+
+    bot.reply_to(message, response)
+    save_user_data()
+
+# Обработка функции забора гифки пользователя   
 @bot.message_handler(content_types=['video', 'animation'])
 def media(message: types.Message):
     gif = r'src\GIF\video_2024-11-17_11-43-49.mp4'
     with open(gif, '+rb') as file:
         bot.send_animation(message.chat.id, file)
 
+# Обработка любого сообщения и изменение количества сообщений отправленных пользователем
 @bot.message_handler(func=lambda message:True, content_types=['text', 'sticker', 'photo', 'video', 'audio', 'document', 'location', 'contact', 'video_note', 'voice'])
 def all_message_handler(message:types.Message):
     user_id = str(message.chat.id)
     today = datetime.date.today()
     week = datetime.date.today().isocalendar()[1]
     month = datetime.date.today().month
-    user_info = 
+    user_info = 1
 
 
+# Обработчик команды "/q" для работы с цитатами
 @bot.message_handler(commands=['q'])
 def quotes_chat(message: types.Message):
-    chat_id = message.chat.id
     if message.reply_to_message:
-        if chat_id not in quotes:
-            quotes[chat_id] = []
         quot = message.reply_to_message.text
-        quotes[chat_id].append(quot)
-        bot.reply_to(message, f'цитата сохранена под номером {len(quotes[chat_id])}')
+        quotes.append(quot)
+        bot.reply_to(message, f'Цитата сохранена под номером {len(quotes)}')
+        save_quotes()
     elif len(message.text.split()) > 1:
         try:
-            a, num = message.text.split()
+            _, num = message.text.split()
             num = int(num) - 1
-            if chat_id in quotes and 0 <= num <len(quotes[chat_id]):
-                selected_q = quotes[chat_id][num]
+            if 0 <= num < len(quotes):
+                selected_q = quotes[num]
                 bot.reply_to(message, f'{selected_q}')
             else:
-                bot.reply_to(message, 'цитата не найдена')
+                bot.reply_to(message, 'Цитата не найдена')
         except (ValueError, IndexError):
-            bot.reply_to(message, f'у тебя аутизм? вот формат гений: /q <номер> (например, /q 2).')
+            bot.reply_to(message, 'Неверный формат команды. Вот правильный формат: /q <номер> (например, /q 2).')
     else:
-        if chat_id in quotes and quotes[chat_id]:
-            response = "\n".join([f"{i + 1}. {quote}" for i, quote in enumerate(quotes[chat_id])])
+        if quotes:
+            response = "\n".join([f"{i + 1}. {quote}" for i, quote in enumerate(quotes)])
             bot.reply_to(message, f"Сохранённые цитаты:\n{response}")
         else:
             bot.reply_to(message, "Нет сохранённых цитат.")
 
-bot.polling()
+# Запуск бота
+if __name__ == '__main__':
+    load_user_data()
+    load_quotes()
+    bot.polling()
